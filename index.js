fe61eedee98e7be0aca5ed158b022dc708a8ae74
Farid <bughunterf@gmail.com>
2024-12-14 20:51:25 +0100

    removing js func replacing it with html&css

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    var navLinks = document.getElementById('nav-links');
-    if (navLinks.style.display === 'flex') {
-        navLinks.style.display = 'none';
-    } else {
-        navLinks.style.display = 'flex';
-    }
-});
